258ad589952913720115daeb564249906a1d2696
Paulocesarhero <56269540+Paulocesarhero@users.noreply.github.com>
2023-04-19 10:44:20 -0600

    Pruebas (#4)
    * Agregue la capa de acceso

---
+++ DataAccessTests/BussinesLogic/TutoriaManagementTests.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tutorias.BussinesLogic.Management;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorias.BussinesLogic.Management.Tests
+{
+    [TestClass()]
+    public class TutoriaManagementTests
+    {
+        [TestMethod()]
+        public void AddCatedraticoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTutorAcademicoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddEstudianteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddExperienciaEducativaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddReporteDeTutoriaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddPeriodoEscolarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModCatedraticoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModExperienciaEducativaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AllocateExperienciaEducativaToCatedraticoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AllocateEstudianteToTutorAcademicoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AllocateSolucionToProblematicaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteCatedraticoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteExperienciaEduactivaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddUSerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCatedraticosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindExperienciaEducativaWithoutCatedraticoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPeriodosEscolaresTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddProblematicaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getProblematicasAcademicasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindProblematicasAcademicasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindTotalAssistantsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getExperienciaEducativaByNRCTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getExperienciasEducativasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddCatedraticosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddSolucionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAcademiasTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetProgramasEducativosTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
